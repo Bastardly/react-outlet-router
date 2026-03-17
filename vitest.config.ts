e2af3bea6e457218466c9c8474499ad4c7607403
Flemming Knøss Hansen (FHA) <fha@kmd.dk>
2026-03-17 15:53:59 +0100

    More changes for the sake of badges

--- vitest.config.ts
+++ vitest.config.ts
@@ -7,5 +7,9 @@ export default defineConfig({
     environment: "jsdom",
     globals: true,
     setupFiles: ["./src/__tests__/setup.ts"],
+    coverage: {
+      provider: "v8",
+      reporter: ["text", "lcov"],
+    },
   },
 });
